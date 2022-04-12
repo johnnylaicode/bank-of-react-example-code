44ad2125f274aea299b40f8ad84b6acfa7959676
KwongSunny <sunnykwong91500@gmail.com>
2022-04-12 18:29:00 -0400

    adding functionality to Debits component

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,17 +1,26 @@
-// src/components/Debits.js
+import { useEffect } from "react/cjs/react.production.min";
 
+// src/components/Debits.js
 const Debits = (props) => {
+
 	let debitsView = () => {
-    const { debits } = props;
-    return debits.map((debit) => {
-      let date = debit.date.slice(0,10);
-      return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
-    }) 
+    // const { debits } = props.data;
+    // debits?.map((debit) => {
+    //   //let date = debit.date.slice(0,10);
+    //   return <li key={debit.id}>TEST</li>
+    // }) 
+
+    return <ul>
+      {props.debits?.map((debit) => {return <li key={debit.id}> {debit.amount} {debit.description} {debit.date}</li>})}
+    </ul>
   }
+
   return (
     <div>
       <h1>Debits</h1>
-      {debitsView()}
+      <h4>Balance: {props.balance}</h4>
+      <h4>Total Debit: {props.totalDebit} | Total Credit: {props.totalCredit}</h4>
+      {props.debits && debitsView()}
       <form onSubmit={props.addDebit}>
         <input type="text" name="description" />
         <input type="number" name="amount" />
